d3f666188cba79d472b198e44e4616331fcced44
rdgrieve <roy.grieve@gmail.com>
2018-05-07 09:33:00 +0100

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -m
+
+mongodb_cmd="mongod"
+#cmd="$mongodb_cmd --httpinterface --rest --master --smallfiles"
+#cmd="$mongodb_cmd --httpinterface --rest --smallfiles"
+cmd="$mongodb_cmd --smallfiles --logpath=/dev/null"
+if [ "$AUTH" == "yes" ]; then
+    cmd="$cmd --auth"
+fi
+
+if [ "$JOURNALING" == "no" ]; then
+    cmd="$cmd --nojournal"
+fi
+
+if [ "$OPLOG_SIZE" != "" ]; then
+    cmd="$cmd --oplogSize $OPLOG_SIZE"
+fi
+
+$cmd &
+
+if [ ! -f /data/db/.mongodb_password_set ]; then
+    /set_mongodb_password.sh
+fi
+
+fg
